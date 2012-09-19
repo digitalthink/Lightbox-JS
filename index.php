7ab13ad23ae0f8096f80e90188c1e42c35dde06e
Denis Allal <denis@digitalthink.fr>
2012-09-19 13:17:48 +0200

    initial commit: created index.php lightbox.js style.css modified notes (TODO) on lightbox.js

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8" />
+	<!-- <link href='http://fonts.googleapis.com/css?family=Bilbo+Swash+Caps' rel='stylesheet' type='text/css'> -->
+
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<!--[if lt IE 8]>
+	<style type="text/css">
+	.wrapper {
+		background: url(bg-transparent.png) repeat;
+    }
+    </style>
+    <![endif]-->
+	<title>The popup in Vanilla JS</title>
+</head>
+<body>
+	<div id="wrap">
+		<h1>The Popup in Vanilla JS.</h1>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<p style="padding:0;margin:0;margin-bottom:20px;"><a id="outside" href="http://www.google.fr/">Go to Google.fr</a></p>
+		<a href="#" rel="lightbox"><img src="nice_ass_thumb.png" alt="Nice Ass." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+		<a href="#" rel="lightbox"><img src="img4_thumb.png" alt="Nice Pose." /></a>
+	</div>
+	<script src="lightbox.js"></script>
+</body>
+</html>
